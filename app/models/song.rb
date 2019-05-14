0525cc7a88941a286d8bb0751e2a0e424f3603f4
github username <github email address>
2019-05-14 14:57:19 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
+  def artist_name #getter and setter help association b/w song and artist this is added to the forms and params
+    self.artist ? self.artist.name : nil
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
